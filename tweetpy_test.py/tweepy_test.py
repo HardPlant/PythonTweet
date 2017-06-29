40bba70a99ee9703cd14c6efab03dfd0c81f009e
unknown <120897@live.jnu.ac.kr>
2017-06-29 18:16:15 +0900

    test successed

--- tweetpy_test.py/tweepy_test.py
+++ tweetpy_test.py/tweepy_test.py
@@ -2,9 +2,33 @@ import tweepy
 import datetime
 consumer_key = ""
 consume_secret = ""
-with open('key.txt') as file:
-    consumer_key = file.readline()
-    consumer_secret = file.readline()
+access_token = ""
+access_token_secret = ""
 
+with open('key.txt') as file:
+    consumer_key = file.readline().rstrip()
+    consumer_secret = file.readline().rstrip()
+    access_token = file.readline().rstrip()
+    access_token_secret = file.readline()
 print(consumer_key)
 print(consumer_secret)
+print(access_token)
+print(access_token_secret)
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+#post Tweet
+tweet = str(datetime.datetime.now()) + ' Test API'
+api.update_status(status=tweet)
+
+print("Successfully Posted.")
+print()
+
+#read Timeline
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
